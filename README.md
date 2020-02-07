68af377c3a0c3acc9a567f0a444199779e8e5822
Nace Zavrtanik <nace1337@gmail.com>
2020-02-07 12:16:18 +0100

    Preverjanje inputa (igralci); README.

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Turnir (custom format)
 
 Naloge:
-- preverjanje inputa (igralci)
 - estetski popravki (tekst, ime formata)
 - berljivost kode (komentarji, imena spremenljivk)
 - razdelitev nagrad glede na prijavnino
--- turnir.py
+++ turnir.py
@@ -5,32 +5,38 @@ from tabulate import tabulate
 
 napaka1 = 'Ups, prišlo je do napake. Vnesi le 1 ali 2: '
 napaka2 = 'Ups, prišlo je do napake. Vnesi ime zmagovalca: '
+napaka3 = 'Ime naj vsebuje le črke in presledke.\n{}. igralec: '
 
 
-def preveri(vnos, opcije, napaka):
-    vnos = string.capwords(vnos.lower())
-    if vnos in opcije:
-        return vnos
+def preveri(vnos, opcije, napaka, tip):
+    if tip == 'ime':
+        if vnos.replace(' ','').isalpha():
+            return string.capwords(vnos)
+        else:
+            return(preveri(input(napaka), None, napaka, 'ime'))      
     else:
-        return(preveri(input(napaka), opcije, napaka))
+        vnos = string.capwords(vnos)
+        if vnos in opcije:
+            return vnos
+        else:
+            return(preveri(input(napaka), opcije, napaka, None))
 
 
 def turnir():
 
     print('================== *** IME LIGE *** ==================\n')
     print('*** OPIS FORMATOV ***\n')
+    print('1 *** Prvi format ***\n2 *** Drugi format ***')
 
     decki = [['Normal', 'Dragon', 'Beast', 'Chaos'], ['Warrior', 'Flip', 'Plant', 'Zombie']]
-    decki = decki[int(preveri(input('1 *** Prvi format *** \n2 *** Drugi format *** \n\nKaterega boste igrali? Številka formata: '), ['1', '2'], napaka1)) - 1]
+    decki = decki[int(preveri(input('\nKaterega boste igrali? Številka formata: '), ['1', '2'], napaka1, None)) - 1]
 
     print('\n*** TEKST ***\n')
 
-    igr1 = string.capwords(input('1. igralec: ').lower())
-    igr2 = string.capwords(input('2. igralec: ').lower())
-    igr3 = string.capwords(input('3. igralec: ').lower())
-    igr4 = string.capwords(input('4. igralec: ').lower())
-
-    igralci = [igr1, igr2, igr3, igr4]
+    igralci = []
+    
+    for i in range(4):
+        igralci.append(preveri(input('{}. igralec: '.format(i+1)), None, napaka3.format(i+1), 'ime'))
 
     print('\nVeliko zabave pri igranju!')
     
@@ -97,8 +103,8 @@ def turnir():
 
         A, B, C, D = [pairingi1[i][0][0].split(' (')[0], pairingi1[i][0][2].split(' (')[0], pairingi1[i][1][0].split(' (')[0], pairingi1[i][1][2].split(' (')[0]]
         
-        zmage1[preveri(input('\nKdo je zmagal, {} ali {}? '.format(A,B)), [A, B], napaka2)] += 1
-        zmage1[preveri(input('Kdo je zmagal, {} ali {}? '.format(C,D)), [C, D], napaka2)] += 1
+        zmage1[preveri(input('\nKdo je zmagal, {} ali {}? '.format(A,B)), [A, B], napaka2, None)] += 1
+        zmage1[preveri(input('Kdo je zmagal, {} ali {}? '.format(C,D)), [C, D], napaka2, None)] += 1
         print('----------------------------------------------------')
 
     zmage2 = sorted(list(zip(zmage1.values(), zmage1.keys())), reverse = 1)
@@ -113,9 +119,9 @@ def turnir():
         
         print(tabulate(table, tablefmt='plain'))
 
-        prvi = preveri(input('\nKdo je zmagal, {} ali {}? '.format(A,B)), [A, B], napaka2)
+        prvi = preveri(input('\nKdo je zmagal, {} ali {}? '.format(A,B)), [A, B], napaka2, None)
         zmage1[prvi] += 1
-        tretji = preveri(input('Kdo je zmagal, {} ali {}? '.format(C,D)), [C, D], napaka2)
+        tretji = preveri(input('Kdo je zmagal, {} ali {}? '.format(C,D)), [C, D], napaka2, None)
         zmage1[tretji] += 1
 
         print('----------------------------------------------------')
@@ -146,8 +152,8 @@ def turnir():
 
         A, B, C, D = [pairingi2[0][0][0].split(' (')[0], pairingi2[0][0][2].split(' (')[0], pairingi2[0][1][0].split(' (')[0], pairingi2[0][1][2].split(' (')[0]]
 
-        zmage1[preveri(input('\nKdo je zmagal, {} ali {}? '.format(A,B)), [A, B], napaka2)] += 1
-        zmage1[preveri(input('Kdo je zmagal, {} ali {}? '.format(C,D)), [C, D], napaka2)] += 1
+        zmage1[preveri(input('\nKdo je zmagal, {} ali {}? '.format(A,B)), [A, B], napaka2, None)] += 1
+        zmage1[preveri(input('Kdo je zmagal, {} ali {}? '.format(C,D)), [C, D], napaka2, None)] += 1
         
         print('----------------------------------------------------')
 
