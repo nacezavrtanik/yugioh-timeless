b46eb52a35741fdfeea80a27c892934da7003ffc
Nace Zavrtanik <nace1337@gmail.com>
2020-02-08 18:52:21 +0100

    Nagradna sporočila.

--- timeless.py
+++ timeless.py
@@ -213,9 +213,9 @@ Veliko zabave pri igranju!
         mozne_porazdelitve = [[4, 2, 1, 1], [3, 3, 2, 0], [3, 2, 2, 1]]
         mozna_mesta = [[1, 2, 3, 3], [1, 1, 3, 4], [1, 2, 2, 4]]
         mozne_nagrade1 = [[10, 6, 2, 2], [8, 8, 4, 0], [9, 5, 5, 1]]
-        mozne_nagrade2 = [['Večna čast in slava.','sporocilo6','sporocilo7','sporocilo7'],
-                          ['sporocilo8','sporocilo8','sporocilo9','sporocilo10']
-                          ['Večna čast in slava.','sporocilo12','sporocilo12','sporocilo13']]
+        mozne_nagrade2 = [['Večna čast in slava.','sporocilo2','sporocilo5','sporocilo5'],
+                          ['sporocilo6','sporocilo6','sporocilo3','sporocilo4']
+                          ['Večna čast in slava.','sporocilo7','sporocilo7','sporocilo4']]
         for i in range(3):
             if porazdelitev == mozne_porazdelitve[i]:
                 mesta = mozna_mesta[i]
