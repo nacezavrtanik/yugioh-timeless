1ccf1cc71721fb28dcf3a663784b577bf9b353fa
Nace Zavrtanik <zavrtaniknace@gmail.com>
2025-04-11 02:10:44 +0200

    Tests for Duelist written

--- src/timeless/duelist.py
+++ src/timeless/duelist.py
@@ -9,6 +9,13 @@ class Duelist:
         self.name = name
         self.win_record = win_record or []
         self.matchup_record = matchup_record or []
+        assert self.name
+        assert len(self.matchup_record) <= 4
+        assert self._compatible_win_and_matchup_records
+
+    def _compatible_win_and_matchup_records(self) -> bool:
+        return len(self.matchup_record) - len(self.win_record) in [0, 1]
+
 
     def __repr__(self):
         return (
@@ -22,34 +29,53 @@ class Duelist:
         return self.name
 
     @property
-    def wins(self):
+    def wins(self) -> int | None:
         if not self.win_record:
-            return 0
+            return None
         return self.win_record[-1]
 
     @property
-    def deck(self):
+    def deck(self) -> str | None:
         if not self.deck_record:
             return None
         return self.deck_record[-1]
 
     @property
-    def opponent(self):
+    def opponent(self) -> str | None:
         if not self.opponent_record:
             return None
         return self.opponent_record[-1]
 
     @property
-    def deck_record(self):
+    def deck_record(self) -> list[str]:
+        if not self.matchup_record:
+            return []
         return [matchup[0] for matchup in self.matchup_record]
 
     @property
-    def opponent_record(self):
+    def opponent_record(self) -> list[str]:
+        if not self.matchup_record:
+            return []
         return [matchup[1] for matchup in self.matchup_record]
 
-    def update_wins(self, won: bool):
-        self.win_record.append(self.wins + int(won))
+    def update_wins(self, won: bool) -> None:
+        assert self.is_dueling
+        wins = 0 if self.wins is None else self.wins
+        self.win_record.append(wins + int(won))
+        assert self._compatible_win_and_matchup_records
 
-    def update_matchup(self, deck: str, opponent: str):
+    def update_matchup(self, deck: str, opponent: str) -> None:
+        assert not self.is_dueling
+        assert deck not in self.deck_record
+        assert opponent not in self.opponent_record or self.round == 4
         assert (deck, opponent) not in self.matchup_record
         self.matchup_record.append((deck, opponent))
+        assert self._compatible_win_and_matchup_records
+
+    @property
+    def round(self) -> int:
+        return len(self.matchup_record)
+
+    @property
+    def is_dueling(self) -> bool:
+        return self.round > len(self.win_record)
---
+++ tests/test_duelist.py
@@ -0,0 +1,96 @@
+
+import pytest
+from hypothesis import strategies as st, given, example
+
+import timeless
+
+
+@st.composite
+def win_records(draw, start=0, step=1, min_size=0, max_size=4):
+    assert max_size <= 4
+    size = draw(st.integers(min_value=min_size, max_value=max_size))
+    records = []
+    while len(records) < size:
+        if records:
+            last_element = records[-1]
+        else:
+            last_element = draw(
+                st.integers(min_value=start, max_value=start + step)
+            )
+        next_element = draw(st.integers(
+            min_value=last_element, max_value=last_element + step
+        ))
+        records.append(next_element)
+    assert len(records) <= 4
+    return records
+
+
+@st.composite
+def matchup_records(draw, min_size=0, max_size=4):
+    assert max_size <= 4
+    size = draw(st.integers(min_value=min_size, max_value=max_size))
+    decks = draw(st.lists(
+        st.text(min_size=1), unique=True, min_size=size, max_size=size
+    ))
+    opponents = draw(st.lists(
+        st.text(min_size=1), unique=True, min_size=size, max_size=size
+    ))
+    if len(opponents) == 4:
+        # Replace final element with a possibly non-unique one
+        opponents[-1] = draw(st.text(min_size=1))
+    records = list(zip(decks, opponents))
+    assert len(records) <= 4
+    return records
+
+
+@st.composite
+def duelists(draw, dueling=None):
+    name = draw(st.text(min_size=1))
+    win_records_max_size = 3 if dueling is True else 4
+    win_record = draw(win_records(max_size=win_records_max_size))
+    win_record_length = len(win_record)
+    if dueling is None:
+        matchup_record_size = dict(
+            min_size=win_record_length,
+            max_size=min([4, win_record_length + 1]),
+        )
+    elif dueling is True:
+        matchup_record_size = dict(
+            min_size=win_record_length + 1,
+            max_size=win_record_length + 1,
+        )
+    elif dueling is False:
+        matchup_record_size = dict(
+            min_size=win_record_length,
+            max_size=win_record_length,
+        )
+    matchup_record = draw(matchup_records(**matchup_record_size))
+    return timeless.Duelist(name, win_record, matchup_record)
+
+
+@given(duelists(dueling=True), st.booleans())
+def test_update_wins(duelist, won):
+    wins = 0 if duelist.wins is None else duelist.wins
+    duelist.update_wins(won)
+    assert duelist.wins == wins + 1 if won else duelist.wins == wins
+
+
+@given(
+    duelists(dueling=False),
+    st.lists(st.text(min_size=1), unique=True, min_size=4),
+    st.lists(st.text(min_size=1), unique=True, min_size=3),
+)
+def test_update_matchup(duelist, decks, opponents):
+    deck = None
+    for deck in decks:
+        if deck not in duelist.deck_record:
+            break
+    opponent = opponents[0]
+    if duelist.round < 4:
+        for opponent in opponents:
+            if opponent not in duelist.opponent_record:
+                break
+    try:
+        duelist.update_matchup(deck, opponent)
+    except Exception as exc_info:
+        pytest.fail(f"Unexpected error: {exc_info}")
